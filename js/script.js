ebf1fb36ed9506bd2c2bb81c3dcdcb3c9c0a4579
munisamee <munisa.ermatova@gmail.com>
2021-07-26 15:17:31 +0200

    added pokemonList
    Three pokemons are added with names, height and types

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-alert('Hello world');
-
-let favoriteFood = 'Mexican';
-document.write(favoriteFood); 
+var pokemonList1 = {
+    name: Bulbasaur,
+    height: 7,
+    types: [ grass, poison],
+}
+var pokemonList2 = {
+    name: Pikachu,
+    height: 4,
+    type: electric
+}
+var pokemonList3 = {
+    name: Butterfree,
+    height: 1,
+    types: [bug, flying],
+}
