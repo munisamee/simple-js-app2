cf2afffc461bf9ae2c2264d2747d532b6d2cc592
munisamee <munisa.ermatova@gmail.com>
2021-07-26 16:27:29 +0200

    index.html and script js edited
    changed from scripts.js to script.js on index. html. Content has changed in script.js file

--- js/script.js
+++ js/script.js
@@ -1,15 +1,4 @@
-var pokemonList1 = {
-    name: Bulbasaur,
-    height: 7,
-    types: [ grass, poison],
-}
-var pokemonList2 = {
-    name: Pikachu,
-    height: 4,
-    type: electric
-}
-var pokemonList3 = {
-    name: Butterfree,
-    height: 1,
-    types: [bug, flying],
-}
+alert('Hello world');
+
+let favoriteFood = 'Mexican';
+document.write(favoriteFood); 
