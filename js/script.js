84fd0ffd015e89302acb7f068b6b72fda842e99a
munisamee <munisa.ermatova@gmail.com>
2021-08-01 17:28:22 +0200

    Condition added
    Condition of "Wow that's big" added to Bulbasaur

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ name: "pikachu", height: "4", type: "electric"}]
 for (let i=0; i < pokemonList.length; i++){
     if (pokemonList[i].height < 10 && pokemonList[i].height > 4) {
         document.write(pokemonList[i].name + "Bulbasaur (height: 7)");
+        console.log(pokemonList[i].name + " Wow that's big")
     }else if (pokemonList[i].height < 6) {
         document.write(pokemonList[i].name + "Pidgey (height: 3)");
     }else {
