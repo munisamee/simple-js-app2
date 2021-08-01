200d137d7a68dbc6e30e68e13336cf7c1bfeedfb
munisamee <munisa.ermatova@gmail.com>
2021-08-01 16:51:56 +0200

    for loop is added
    for loop is added for all pokemons in the list

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 let pokemonList = [{name: "bulbasaur", height: "7", type: ["grass", "poison"],
 name: "pidgey", height: "3", type: ["flying", "normal"],
 name: "pikachu", height: "4", type: "electric"}]
+
+
+for (let i=0; i < pokemonList.length; i++){
+    if (pokemonList[i].height < 10 && pokemonList[i].height > 4) {
+        document.write(pokemonList[i].name + "Bulbasaur (height: 7)");
+    }else if (pokemonList[i].height < 6) {
+        document.write(pokemonList[i].name + "Pidgey (height: 3)");
+    }else {
+        document.write(pokemonList[i].name + "Pikachu (height: 4)");
+    }
+}
