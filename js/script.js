6ab26d8a0390ae42a95f4489b196f5c3d73cb86b
munisamee <munisa.ermatova@gmail.com>
2021-08-02 21:36:41 +0200

    height and for loop changed
    Height for each pokemon is changed and as well as document.write inside for loop is edited

--- js/script.js
+++ js/script.js
@@ -1,29 +1,33 @@
 let pokemonList = [
 {
     name: "bulbasaur", 
-    height: "7", 
+    height: "0.7", 
     type: ["grass", "poison"],
 },
 
 {
     name: "pidgey", 
-    height: "3", 
+    height: "1.7", 
     type: ["flying", "normal"],
 },
 
 {
     name: "pikachu", 
-    height: "4", 
+    height: "44", 
     type: "electric",
 }
 ]
 
 /** Here add for loop for all pokemons! */
 
-for (let i=0; i < pokemonList.length; i++){
-    if (pokemonList[i].height < 10) {
-        document.write(pokemonList[i].name + "Bulbasaur (height: 7)", "<br");
-        console.log(pokemonList[i].name + " Wow that's big")
+for (let i = 0; i < pokemonList.length; i++){
+    console.log("pokemonList", pokemonList[i].name);
+
+    if (pokemonList[i].height > 10) {
+        document.write(
+            pokemonList[i].name + "Bulbasaur (height: 7) Wow that's big", 
+            "<br"
+            );
     }else if (pokemonList[i].height < 6) {
         document.write(pokemonList[i].name + "Pidgey (height: 3)", "<br>");
     }else {
