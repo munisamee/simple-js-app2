4741163e240c0362a2808dd89290b65e684b8d5a
munisamee <munisa.ermatova@gmail.com>
2021-08-08 19:58:25 +0200

    function IIFE updated

--- js/script.js
+++ js/script.js
@@ -1,50 +1,67 @@
-let pokemonList = [
-{
-    name: "bulbasaur", 
-    height: "0.7", 
-    type: ["grass", "poison"],
-},
+/** Add new pokemon repository here */
 
-{
-    name: "pidgey", 
-    height: "1.7", 
-    type: ["flying", "normal"],
-},
+let pokemonRepository = (function() {
+    let pokemonList = [
+            {
+                name: "bulbasaur", 
+                height: "0.7", 
+                type: ["grass", "poison"],
+            },
+            
+            {
+                name: "pidgey", 
+                height: "1.7", 
+                type: ["flying", "normal"],
+            },
+            
+            {
+                name: "pikachu", 
+                height: "44", 
+                type: "electric",
+            }
+        ];
+        function add(pokemon) {
+            pokemonList.push(pokemon);
+        }
+    
+        function getAll() {
+            return pokemonList;
+        }
+    
+        return {
+            add: add,
+            getAll: getAll
+        };
+    })();
+    
 
-{
-    name: "pikachu", 
-    height: "44", 
-    type: "electric",
-}
-]
 
-/** Here add for loop for all pokemons! */
+/** Here add forEach instead of for loop for all pokemons! */
 
 pokemonList.forEach(function(pokemon) {
-    console.log(pokemonList[i].name + ' is' + pokemonList[i].height + ' cm' + pokemonList[i].type + ' ');
+    console.log(pokemonList[i].name + ' is' + pokemonList[i].height + ' inch' + pokemonList[i].type + ' ');
 });
 
-
-function pokemonList() {
-    let pokemonList = {};
+if (pokemonList[i].height > 10) {
+    document.write(
+        pokemonList[i].name + "Bulbasaur (height: 7) Wow that's big", 
+        "<br"
+        );
+}else if (pokemonList[i].height < 6) {
+    document.write(pokemonList[i].name + "Pidgey (height: 3)", "<br>");
+}else {
+    document.write(pokemonList[i].name + "Pikachu (height: 4)", "<br>");
 }
-console.log(pokemonList);
 
-let pokemonRepository = (function () {
-    let pokemonList = [];
+(function() {
+    console.log(pokemonList);
+}());
 
-    function add(pokemon) {
-        pokemonList.push(pokemon);
-    }
 
-    function getAll() {
-        return pokemonList;
-    }
 
-    return {
-        add: add,
-        getAll: getAll
-    };
-})();
+
+
+
+
 
 
